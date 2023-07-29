c4684e09745459ab20ab0e5676eada46b4819906
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-07-29 16:53:32 -0300

    Project initialization.

---
+++ fhir-terminology-generator.js
@@ -0,0 +1,10 @@
+// For clearing the terminal output.
+import clear from 'clear';
+// For accessing the UI functionalities.
+import * as ui from './lib/ui.js';
+
+// Clear the terminal screen.
+clear();
+
+// Show the application title.
+ui.showTitle();
