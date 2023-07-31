4916a74e13dfeea4c6770a1c6435687131027ee1
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-07-31 17:51:13 -0300

    Added CSV content for testing.

--- fhir-terminology-generator.js
+++ fhir-terminology-generator.js
@@ -2,9 +2,15 @@
 import clear from 'clear';
 // For accessing the UI functionalities.
 import * as ui from './lib/ui.js';
+// For CSV to JSON conversion.
+import csv from 'csvtojson';
 
 // Clear the terminal screen.
 clear();
 
 // Show the application title.
 ui.showTitle();
+
+// Testing the CSV loading.
+const jsonArray = await csv().fromFile('tests/fixtures/cbo-grande-grupo.csv');
+console.log(jsonArray);
