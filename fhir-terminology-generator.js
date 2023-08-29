dd8dd3364e5c69ad4f55c0599eb7e0a3c809c00f
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-08-29 17:09:12 -0300

    Added yargs.

--- fhir-terminology-generator.js
+++ fhir-terminology-generator.js
@@ -2,6 +2,10 @@
 import clear from 'clear';
 // For accessing the UI functionalities.
 import * as ui from './lib/ui.js';
+// For parsing command line arguments.
+import _yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+const yargs = _yargs(hideBin(process.argv));
 // For CSV to JSON conversion.
 import csv from 'csvtojson';
 // For reading/writing content from/to the filesystem.
@@ -15,6 +19,26 @@ clear();
 // Show the application title.
 ui.showTitle();
 
+// ???
+const options = yargs
+    .usage("Usage: --status <status>")
+    .option("status", {
+        describe: "The status of the code system",
+        type: "string",
+        choices: ['draft', 'active', 'retired', 'unknown'],
+        demandOption: true
+    })
+    .option("content", {
+        describe: "The extent of the content of the code system",
+        type: "string",
+        choices: ['not-present', 'example', 'fragment', 'complete', 'supplement'],
+        demandOption: true
+    })
+    .argv;
+
+
+
+/*
 // Load the CSV as JSON.
 const jsonArray = await csv().fromFile('tests/fixtures/cbo-grande-grupo.csv');
 //console.log(jsonArray);
@@ -29,3 +53,4 @@ const filledTemplate = Mustache.render(template, {
     concept: JSON.stringify(jsonArray, null, 4)
 });
 console.log(filledTemplate);
+*/
