a8820feee365fcd9ceb5e62931ed03e0fce41169
alexandre-savaris <109041080+alexandre-savaris@users.noreply.github.com>
2023-09-26 14:10:01 -0300

    Refactoring.

--- src/fhir-terminology-generator.js
+++ src/fhir-terminology-generator.js
@@ -5,11 +5,13 @@ import * as ui from '../lib/ui.js';
 // For parsing command line arguments.
 import _yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
-const yargs = _yargs(hideBin(process.argv));
+let yargs = _yargs(hideBin(process.argv));
+// For common definitions regarding CodeSystems e ValueSets.
+import * as common from '../lib/common.js';
 // For CodeSystem generation.
-import * as codesystem from '../lib/CodeSystem.js'
+import * as codesystem from '../lib/CodeSystem.js';
 // For ValueSet generation.
-import * as valueset from '../lib/ValueSet.js'
+import * as valueset from '../lib/ValueSet.js';
 
 // Clear the terminal screen.
 clear();
@@ -17,104 +19,13 @@ clear();
 // Show the application title.
 ui.showTitle();
 
-// Execute the CLI.
+// Configure commands.
 yargs
     .command(codesystem)
-    .command(valueset)
-    .option('release', {
-        alias: 'r',
-        describe: 'Release of FHIR to be used.',
-        type: 'string',
-        choices: ['R5'],
-        default: 'R5',
-        demandOption: true
-    })
-    .option('url', {
-        alias: 'u',
-        describe: 'Canonical identifier for this terminology.',
-        type: 'string',
-        demandOption: false
-    })
-    .option('vers', {
-        alias: 'v',
-        describe: 'Business version of the terminology.',
-        type: 'string',
-        demandOption: false
-    })
-    .option('name', {
-        alias: 'n',
-        describe: 'Name for this terminology (computer friendly)',
-        type: 'string',
-        demandOption: false
-    })
-    .option('title', {
-        alias: 't',
-        describe: 'Name for this terminology (human friendly)',
-        type: 'string',
-        demandOption: false
-    })
-    .option('status', {
-        alias: 's',
-        describe: 'The status of the terminology',
-        type: 'string',
-        choices: ['draft', 'active', 'retired', 'unknown'],
-        demandOption: true
-    })
-    .option('experimental', {
-        alias: 'x',
-        describe: 'For testing purposes, not real usage',
-        type: 'string',
-        choices: ['true', 'false'],
-        demandOption: false
-    })
-    .option('date', {
-        alias: 'd',
-        describe: 'Date last changed (in the format YYYY, YYYY-MM, YYYY-MM-DD or YYYY-MM-DDThh:mm:ss+zz:zz)',
-        type: 'string',
-        demandOption: false
-    })
-    .option('publisher', {
-        alias: 'p',
-        describe: 'Name of the publisher/steward (organization or individual)',
-        type: 'string',
-        demandOption: false
-    })
-    .option('description', {
-        alias: 'e',
-        describe: 'Natural language description of the terminology',
-        type: 'string',
-        demandOption: false
-    })
-    .option('purpose', {
-        alias: 'w',
-        describe: 'Why this terminology is defined',
-        type: 'string',
-        demandOption: false
-    })
-    .option('copyright', {
-        alias: 'y',
-        describe: 'Use and/or publishing restrictions',
-        type: 'string',
-        demandOption: false
-    })
-    .option('copyrightLabel', {
-        alias: 'L',
-        describe: 'Copyright holder and year(s)',
-        type: 'string',
-        demandOption: false
-    })
-    .option('approvalDate', {
-        alias: 'D',
-        describe: 'When the terminology was approved by publisher (in the format YYYY, YYYY-MM, or YYYY-MM-DD)',
-        type: 'string',
-        demandOption: false
-    })
-    .option('lastReviewDate', {
-        alias: 'R',
-        describe: 'When the terminology was last reviewed by the publisher (in the format YYYY, YYYY-MM, or YYYY-MM-DD)',
-        type: 'string',
-        demandOption: false
-    })
+    .command(valueset);
+
+// Configure options unrelated to the structure of CodeSystem or ValueSet resources.
+yargs
     .option('input-csv-file', {
         alias: 'i',
         describe: 'Path to the input CSV file.',
@@ -126,6 +37,12 @@ yargs
         describe: 'Path to the output JSON file.',
         type: 'string',
         demandOption: true
-    })
+    });
+
+// Retrieve options that are common to CodeSystems and ValueSets.
+yargs = common.defineCommonOptions(yargs);
+
+// Execute the CLI.
+yargs
     .help()
     .argv;
